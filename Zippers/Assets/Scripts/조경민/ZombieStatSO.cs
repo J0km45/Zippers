82b3ba4ae602371bacf6064132bab24c70751fca
Jo Kyeongmin <skykm0405@naver.com>
2026-04-24 14:34:40 +0900

    [feat] ZombieStatSO 생성
    ZombieStatSO 생성

---
+++ Zippers/Assets/Scripts/조경민/ZombieStatSO.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+// Create -> 메뉴: Assets > Create > Zippers > Zombie Stat Data
+[CreateAssetMenu(menuName = "Zippers/Zombie Stat Data")]
+public class ZombieStatSO : ScriptableObject
+{
+    [Tooltip("좀비 타입")]
+    public ZombieType Type;
+    [Tooltip("최대 체력")]
+    public int MaxHp;
+    [Tooltip("이동 속도")]
+    public float MoveSpeed;
+    [Tooltip("공격력")]
+    public int AttackDamage;
+    [Tooltip("공격 간격")]
+    public float AttackCooldown;
+    [Tooltip("공격 범위")]
+    public float AttackRange;
+    [Tooltip("감지 범위")]
+    public float DetectRange;
+    [Tooltip("처치 재화")]
+    public float KillReward;
+}
+
+public enum ZombieType
+{
+    Normal,
+}
