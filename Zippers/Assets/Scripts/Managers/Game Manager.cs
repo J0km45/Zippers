ad76cc3f9baf274be3f3968722b988c4470d26ff
Hooni0_0 <startrack0515@gmail.com>
2026-04-27 21:14:29 +0900

    [refactor] 프리팹도 싱글톤화하는 메서드 추가
    - Auido Manager Prefab 프리팹화 하기 위함

--- Zippers/Assets/Scripts/Managers/Game Manager.cs
+++ Zippers/Assets/Scripts/Managers/Game Manager.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     private static GameManager _instance;
-
-    [SerializeField] private bool _debugAllOn = true;
+    
+    [SerializeField] private AudioManager _audioManager;
     
     private void Awake()
     {
@@ -23,24 +23,37 @@ public class GameManager : MonoBehaviour
     private void Init()
     {
         GenerateManager<GameSceneManager>();
-        GenerateManager<AudioManager>();
+        GenerateManager(_audioManager);
         GenerateManager<TimeScaleManager>();
-        
-        DebugTool.DebugPrintAll(_debugAllOn);
     }
 
     private void Start()
-    {
-        DebugTool.Log("게임 시작", DebugType.Game, this);
-    }
+        => DebugTool.Log("게임 시작", DebugType.Game, this);
 
     private void GenerateManager<T>() where T : Component
     {
-        if (FindAnyObjectByType<T>() != null) return;
+        if (FindAnyObjectByType<T>() != null)
+        {
+            DebugTool.Warning($"{typeof(T).Name} 을 로드하지 못했습니다.", DebugType.Game, this);
+            return;
+        }
         
         var go = new GameObject(typeof(T).Name);
         go.AddComponent<T>();
         DontDestroyOnLoad(go);
+    }
+    
+    private void GenerateManager<T>(T managerPrefab) where T : Component
+    {
+        if (FindAnyObjectByType<T>() != null)
+        {
+            DebugTool.Warning($"{typeof(T).Name} 프리팹을 로드하지 못했습니다.", DebugType.Game, this);
+            return;
+        }
+        
+        T manager = Instantiate(managerPrefab);
+        manager.gameObject.name = typeof(T).Name;
+        DontDestroyOnLoad(manager.gameObject);
     }
 }
 
