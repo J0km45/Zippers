df2467cc5c498a7b0c9da58205cc5e4b23cdc1cf
Hooni0_0 <startrack0515@gmail.com>
2026-04-27 21:11:26 +0900

    [feat] 각 오디오 컨트롤러의 부모 클래스 추가
    - SFX 재생, BGM 재생, 랜덤 clip 메서드

---
+++ Zippers/Assets/Scripts/박승훈/Audio/AudioController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Audio
+{
+    public class AudioController : MonoBehaviour
+    {
+        protected void PlaySFX(AudioSource source, List<AudioClip> clips)
+        {
+            AudioClip clip = RandomPlay(clips);
+            source.PlayOneShot(clip);
+        }
+        
+        protected void PlaySFX(AudioSource source, AudioClip clip)
+            => source.PlayOneShot(clip);
+
+        protected void PlayBGM(AudioSource source, List<AudioClip> clips)
+        {
+            AudioClip clip = RandomPlay(clips);
+            source.clip = clip;
+            source.Play();
+        }
+        
+        private AudioClip RandomPlay(List<AudioClip> clips)
+        {
+            int random = Random.Range(0, clips.Count - 1);
+            return clips[random];
+        }
+
+        protected void SfxSourceInit(AudioSource source)
+        {
+            source.playOnAwake = false;
+            source.loop = false;
+        }
+
+        protected void BgmSourceInit(AudioSource source)
+        {
+            source.playOnAwake = false;
+            source.loop = true;
+        }
+    }
+}
