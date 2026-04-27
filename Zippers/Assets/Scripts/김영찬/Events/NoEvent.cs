dd009638b10c1c55d3e49c04490adc1b13174383
abcbbq99 <kim3373613@gmail.com>
2026-04-27 14:04:38 +0900

    [update] NoEvent SO파일에 들어가는 NoEvevt.cs를 프리펩화하여 SO에 삽입

---
+++ Zippers/Assets/Scripts/김영찬/Events/NoEvent.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class NoEvent : NodeEvent
+{
+    public override void EventEnter()
+    {
+        
+    }
+
+    public override void EventUpdate()
+    {
+        
+    }
+
+    public override void EventExit()
+    {
+        
+    }
+}
