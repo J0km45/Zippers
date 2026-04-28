c32a5e3e94da3245f0e4a722ae4ff3f16562a00a
Hooni0_0 <startrack0515@gmail.com>
2026-04-28 14:30:18 +0900

    [feat] 좀비 SFX SO, Controller 추가
    - SO : 걷기, 공격, 피격, 죽음, 재화 드랍 SFX 추가
    - Controller : 각 SFX 재생 메서드 추가

--- Zippers/Assets/Scripts/박승훈/Audio/Controllers/ZombieSfxController.cs
+++ Zippers/Assets/Scripts/박승훈/Audio/Controllers/ZombieSfxController.cs
@@ -1,8 +1,55 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Audio
 {
     public class ZombieSfxController : AudioController
     {
+        [Header("좀비 오디오 소스 컴포넌트")]
+        [SerializeField] private AudioSource _audioSource;
+        [Space(5)] [Header("좀비 SFX SO")]
+        [SerializeField] private ZombieSFXSO _zombieSFXSO;
+
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+            
+            _zombieSFXSO.ZombieSfxDictInit();
+        }
+
+        public void PlayMoveSfx()
+        {
+            List<AudioClip> clips = _zombieSFXSO.MoveSfx;
+            PlaySFX(_audioSource,clips);
+            DebugTool.Log("좀비 이동 SFX 재생", DebugType.Audio, this);
+        }
+
+        public void PlayAttackSfx(ZombieType type)
+        {
+            List<AudioClip> clips = _zombieSFXSO.AttackSfxDict[type];
+            PlaySFX(_audioSource,clips);
+            DebugTool.Log($"좀비 공격 {type.ToString()} SFX 재생", DebugType.Audio, this);
+        }
+
+        public void PlayHitSfx()
+        {
+            List<AudioClip> clips = _zombieSFXSO.HitSfx;
+            PlaySFX(_audioSource,clips);
+            DebugTool.Log("좀비 피격 SFX 재생", DebugType.Audio, this);
+        }
+
+        public void PlayDeathSfx()
+        {
+            List<AudioClip> clips = _zombieSFXSO.DeathSFX;
+            PlaySFX(_audioSource,clips);
+            DebugTool.Log("좀비 죽음 SFX 재생", DebugType.Audio, this);
+        }
+
+        public void PlayDropResourcesSfx(ResourcesType type)
+        {
+            List<AudioClip> clips = _zombieSFXSO.ResourcesSfxDict[type];
+            PlaySFX(_audioSource,clips);
+            DebugTool.Log($"좀비 자원 {type.ToString()} 드랍 SFX 재생", DebugType.Audio, this);
+        }
     }
 }
