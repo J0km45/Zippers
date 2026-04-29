22701cad4d777fc6f06f23d6342a4665f6076e17
Hooni0_0 <startrack0515@gmail.com>
2026-04-29 23:27:44 +0900

    [fix] 경로 이동 과정중 연결 해제된 audio 연결

--- Zippers/Assets/Scripts/박승훈/Audio/AudioSO/BGMSO.cs
+++ Zippers/Assets/Scripts/박승훈/Audio/AudioSO/BGMSO.cs
@@ -49,5 +49,7 @@ public class BGMSO : ZippersSO
         _BGMDict.Add(BGMType.Shop, _shop);
         _BGMDict.Add(BGMType.Clear, _clear);
         _BGMDict.Add(BGMType.Fail, _fail);
+        
+        DebugTool.Log("BGM 딕셔너리 초기화 완료", DebugType.Zombie);
     }
 }
