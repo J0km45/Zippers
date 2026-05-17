9e54fa63ecbcf20855f52b854b0816b6b643f189
Hooni0_0 <startrack0515@gmail.com>
2026-05-18 01:32:54 +0900

    [feat] UI 오디오 컨틀로러 추가
    - 전투 시작, 클리어, 다음 웨이브 오디오

--- Zippers/Assets/Scripts/박승훈/Audio/Controllers/UIController.cs
+++ Zippers/Assets/Scripts/박승훈/Audio/Controllers/UIController.cs
@@ -5,5 +5,37 @@ namespace Audio
 {
     public class UIController : AudioController
     {
+        [Header("UI 오디오 소스 컴포넌트")]
+        [SerializeField] private AudioSource _audioSource;
+        
+        [Space(5)] [Header("UI SFX SO")]
+        [SerializeField] private UISFXSO _uiSFXSO;
+
+        private void Awake()
+            => _audioSource = GetComponent<AudioSource>();
+
+        private void Start()
+            => SfxSourceInit(_audioSource);
+
+        public void PlayBattleStart()
+        {
+            AudioClip clip = _uiSFXSO.StageClear;
+            PlaySFX(_audioSource, clip);
+            DebugTool.Log("스테이지 시작 SFX 재생", DebugType.Audio, this);
+        }
+
+        public void PlayBattleClear()
+        {
+            AudioClip clip = _uiSFXSO.StageClear;
+            PlaySFX(_audioSource, clip);
+            DebugTool.Log("스테이지 클리어 SFX 재생", DebugType.Audio, this);
+        }
+
+        public void PlayNextWave()
+        {
+            AudioClip clip = _uiSFXSO.NextWave;
+            PlaySFX(_audioSource, clip);
+            DebugTool.Log("다음 SFX 재생", DebugType.Audio, this);
+        }
     }
 }
